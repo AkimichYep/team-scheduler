781c977dd47410fc6a62c78d1195af0261689b14
Maksym <maxim.yep@gmail.com>
2026-06-11 19:32:06 +0300

    Added sql init

--- init.sql
+++ init.sql
@@ -1,11 +1,8 @@
--- Create database if it doesn't exist
-CREATE DATABASE team_scheduler;
+-- Create user if it doesn't exist
+CREATE USER scheduler_user WITH PASSWORD 'scheduler_password';
 
--- Grant privileges to the scheduler_user
+-- Create database
+CREATE DATABASE team_scheduler OWNER scheduler_user;
+
+-- Grant privileges
 GRANT ALL PRIVILEGES ON DATABASE team_scheduler TO scheduler_user;
-
--- Connect to the database and grant schema privileges
-\c team_scheduler
-GRANT ALL ON SCHEMA public TO scheduler_user;
-ALTER DEFAULT PRIVILEGES IN SCHEMA public GRANT ALL ON TABLES TO scheduler_user;
-ALTER DEFAULT PRIVILEGES IN SCHEMA public GRANT ALL ON SEQUENCES TO scheduler_user;
